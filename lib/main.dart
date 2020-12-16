6de1b396efc59a9219633953d8c092ff04496e49
Tim Friedrich <tim@pop-os.localdomain>
2020-12-16 22:09:36 +0100

    gesture detector

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart'; //## iOS-Design-Language
 import 'package:flutter/material.dart'; //## Farben, Icons, etc. (Design)
 import 'package:flutter/painting.dart'; //## Schatten, Farben, etc. (spezifischer)
 import 'package:flutter/rendering.dart'; //## Render-Zeug
+import 'package:stundenplaner/stateStundenplan.dart';
 
 import 'nicerStyle.dart'; //## Importiert eigenes Style-File
 import 'state_Regulator.dart'; //## Importiert den State/Tab-Regulator
@@ -10,96 +11,122 @@ import 'settings.dart';
 
 import 'package:bottom_navy_bar/bottom_navy_bar.dart'; //## BottomBar
 
+void main() => runApp(MaterialApp(
+      //## Startet App, Setzt StartWidget
+      home: AppHome(),
+      //home: DefaultBottomBarController(child: Vertretungsplan()),
+    ));
 
-
-
-void main() => runApp(MaterialApp( //## Startet App, Setzt StartWidget
-		home: AppHome(),
-    //home: DefaultBottomBarController(child: Vertretungsplan()),
-));
-class AppHome extends StatelessWidget{ //## StartWidget, Home = NavBar
+class AppHome extends StatelessWidget {
+  //## StartWidget, Home = NavBar
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NavBar(), //## NavBar 
+      home: NavBar(), //## NavBar
     );
   }
-} 
-class NavBar extends StatefulWidget{ //## 
+}
+
+class NavBar extends StatefulWidget {
+  //##
   @override
   _NavBarState createState() => _NavBarState();
 }
 
-
-class _NavBarState extends State<NavBar>{
+class _NavBarState extends State<NavBar> {
   int currentIndex = 1; //## Legt den Start-Index fest (Start-Tab)
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          //## AppBar-Settings
+          //leading: Icon(Icons.menu),
+          title: barStates[currentIndex],
+          actions: <Widget>[
+            Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.error_outline_sharp,
+                          color: Colors.black, size: 25),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => Report()));
+                      },
+                    ),
+                    //SizedBox(width: 15,),
+                    IconButton(
+                      icon: Icon(Icons.settings_outlined,
+                          color: Colors.black, size: 25),
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => Settings()));
+                      },
+                    ),
+                  ],
+                ))
+          ],
+          backgroundColor: Colors.white,
+          elevation: 0, //## Lässt den Schwebeeffekt/Schatten verschwinden
+        ),
+        body: bodyStates[
+            currentIndex], //## Ruft jeweiligen State/Tag für den Body auf
 
-      appBar: AppBar( //## AppBar-Settings
-        //leading: Icon(Icons.menu),
-        title: barStates[currentIndex],
-        actions: <Widget>[Padding(padding: EdgeInsets.fromLTRB(0,0,15,0), 
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    IconButton(
-                                      icon: Icon(Icons.error_outline_sharp, color: Colors.black, size: 25),
-                                      onPressed: (){
-                                        Navigator.of(context).push(
-                                          MaterialPageRoute(builder: (context) => Report())
-                                        );
-                                      },
-                                    ),
-                                    //SizedBox(width: 15,),
-                                    IconButton(
-                                      icon: Icon(Icons.settings_outlined, color: Colors.black, size: 25),
-                                      onPressed: (){
-                                        Navigator.of(context).push(
-                                          MaterialPageRoute(builder: (context) => Settings())
-                                        );
-                                      },
-                                    ),
-                                ],))],
-        backgroundColor: Colors.white,
-        elevation: 0, //## Lässt den Schwebeeffekt/Schatten verschwinden
-      ),
-
-      body: bodyStates[currentIndex], //## Ruft jeweiligen State/Tag für den Body auf
-
-      bottomNavigationBar: GestureDetector(
-        /*
-        onHorizontalDragUpdate: (DragUpdateDetails dragUpdateDetails){
-          if(dragUpdateDetails > 0){currentIndex++}
-          print(primayDelta);
-        },
-        */
-        child: BottomNavyBar( //## BottomBar-Settings
-          showElevation: false,
-          animationDuration: Duration(milliseconds: 420),
-          curve: Curves.easeInOut,
-          mainAxisAlignment: MainAxisAlignment.spaceAround, //## Rand um die Items
-          selectedIndex: currentIndex, //## Erkennt den aktuellen Index (int, s.o.)
-          onItemSelected: (index){ //## Ändert den aktuellen Index bei Knopfdruck
-            setState((){
-              currentIndex = index;
-            });},
-          items: <BottomNavyBarItem>[ //## Der Inhalt der BottomBar (Items)
-            BottomNavyBarItem(
-              icon: Icon(Icons.date_range_sharp), title: Text("Stundenplan", style: niceBar,), 
-              activeColor: Colors.redAccent, inactiveColor: Colors.black, textAlign: TextAlign.center),
-            BottomNavyBarItem(
-              icon: Icon(Icons.home_outlined), title: Text("Mein Tag", style: niceBar), 
-              activeColor: Colors.blueAccent, inactiveColor: Colors.black, textAlign: TextAlign.center),
-            BottomNavyBarItem(
-              icon: Icon(Icons.list_alt_sharp), title: Text("Vertretung", style: niceBar), 
-              activeColor: Colors.greenAccent[700], inactiveColor: Colors.black, textAlign: TextAlign.center),
-          ],),
-      )
-      
-
-    );
+        bottomNavigationBar: GestureDetector(
+          onPanUpdate: (details) {
+            if (details.delta.dx > 0) {
+              setState(() {
+                currentIndex++;
+              });
+            } else if (details.delta.dx < 0) {
+              setState(() {
+                currentIndex--;
+              });
+            }
+          },
+          child: BottomNavyBar(
+            //## BottomBar-Settings
+            showElevation: false,
+            animationDuration: Duration(milliseconds: 420),
+            curve: Curves.easeInOut,
+            mainAxisAlignment:
+                MainAxisAlignment.spaceAround, //## Rand um die Items
+            selectedIndex:
+                currentIndex, //## Erkennt den aktuellen Index (int, s.o.)
+            onItemSelected: (index) {
+              //## Ändert den aktuellen Index bei Knopfdruck
+              setState(() {
+                currentIndex = index;
+              });
+            },
+            items: <BottomNavyBarItem>[
+              //## Der Inhalt der BottomBar (Items)
+              BottomNavyBarItem(
+                  icon: Icon(Icons.date_range_sharp),
+                  title: Text(
+                    "Stundenplan",
+                    style: niceBar,
+                  ),
+                  activeColor: Colors.redAccent,
+                  inactiveColor: Colors.black,
+                  textAlign: TextAlign.center),
+              BottomNavyBarItem(
+                  icon: Icon(Icons.home_outlined),
+                  title: Text("Mein Tag", style: niceBar),
+                  activeColor: Colors.blueAccent,
+                  inactiveColor: Colors.black,
+                  textAlign: TextAlign.center),
+              BottomNavyBarItem(
+                  icon: Icon(Icons.list_alt_sharp),
+                  title: Text("Vertretung", style: niceBar),
+                  activeColor: Colors.greenAccent[700],
+                  inactiveColor: Colors.black,
+                  textAlign: TextAlign.center),
+            ],
+          ),
+        ));
   }
 }
