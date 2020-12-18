c15bc471c2495746418194294b5646b95b5c5571
Tim Friedrich <tim.david007@gmail.com>
2020-12-18 10:54:01 +0100

    Design (Vertrung)

--- lib/stateVertretungsplan.dart
+++ lib/stateVertretungsplan.dart
@@ -13,6 +13,58 @@ stateVertretungsplanBarTitle() {
 
 stateVertretungsplanBody() {
   return Scaffold(
+    body: Container(
+        color: Color.fromRGBO(0, 0, 0, 0.05), //## Hintergrundfarbe
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              //## Erstellt für eine dynamische Häufigkeit die Eintröge
+              child: ListView.builder(
+                padding: EdgeInsets.fromLTRB(
+                    15, 0, 15, 15), //## Ränder um Gesamtliste
+                itemCount:
+                    vertretung["klasse"].length, //## Misst Länge für Einträge
+                itemBuilder: (context, i) {
+                  //## Geht jede Zeile durch und schreibt den Eintrag
+                  return vertretungsEintrag(i.toInt());
+                },
+              ),
+            ),
+          ],
+        )),
+    floatingActionButton: FlatButton(
+      onPressed: () {
+        print("hi");
+      },
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.fromLTRB(10, 15, 10, 0),
+        child: Container(
+          //padding: EdgeInsets.fromLTRB(15, 15, 15, 0),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40), color: Colors.grey[700]),
+          child: ListTile(
+            //## Datums-Leiste
+            //contentPadding: EdgeInsets.all(0),
+            //tileColor: Colors.black12,
+            leading: Icon(Icons.arrow_left, color: Colors.white),
+            title: Text(
+              "Montag, 14. Dezember 2020",
+              style: niceSubtitle,
+              textAlign: TextAlign.center,
+            ),
+            trailing: Icon(Icons.arrow_right, color: Colors.white),
+          ),
+        ),
+      ),
+    ),
+    floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+  );
+}
+
+stateVertretungsplanBody2() {
+  return Scaffold(
       body: Container(
           color: Color.fromRGBO(0, 0, 0, 0.05), //## Hintergrundfarbe
           child: Column(
@@ -21,7 +73,7 @@ stateVertretungsplanBody() {
               ListTile(
                 //## Datums-Leiste
                 //contentPadding: EdgeInsets.all(0),
-                tileColor: Colors.black87,
+                tileColor: Colors.black12,
                 leading: Icon(Icons.arrow_left, color: Colors.white),
                 title: Text(
                   "Montag, 14. Dezember 2020",
