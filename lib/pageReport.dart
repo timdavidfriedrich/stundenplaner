88b6ac07033b919e0c970d5ca0ef0918840f54a7
Tim Friedrich <tim.david007@gmail.com>
2021-01-05 12:16:11 +0100

    Firebase-Integration

--- lib/pageReport.dart
+++ lib/pageReport.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:data_connection_checker/data_connection_checker.dart';
 
 import '.nicerStyle.dart';
